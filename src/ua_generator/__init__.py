d3093f8c1d935b72e5116d716c3247b1b3a6d50c
iamdual <imduual@gmail.com>
2024-08-12 20:45:34 +0300

    type hinting for generator()

--- src/ua_generator/__init__.py
+++ src/ua_generator/__init__.py
@@ -3,8 +3,13 @@ Random User-Agent
 Copyright: 2022-2024 Ekin Karadeniz (github.com/iamdual)
 License: Apache License 2.0
 """
-from . import user_agent
+import typing
+
+from . import user_agent, options as _options
 
 
-def generate(**kwargs):
-    return user_agent.UserAgent(**kwargs)
+def generate(device: typing.Union[tuple, str, None] = None,
+             platform: typing.Union[tuple, str, None] = None,
+             browser: typing.Union[tuple, str, None] = None,
+             options: typing.Union[_options.Options, None] = None) -> user_agent.UserAgent:
+    return user_agent.UserAgent(device, platform, browser, options)
