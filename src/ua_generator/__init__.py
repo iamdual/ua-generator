8a564943ea6acdd0463a51ff8dd408eed2ad701e
iamdual <3048763+iamdual@users.noreply.github.com>
2024-10-20 17:21:30 +0300

    Revert "Typing literals + UserAgent __repr__"

--- src/ua_generator/__init__.py
+++ src/ua_generator/__init__.py
@@ -5,13 +5,11 @@ License: Apache License 2.0
 """
 import typing
 
-from . import user_agent, options as _options, data as _data
+from . import user_agent, options as _options
 
 
-def generate(
-    device: typing.Union[tuple[_data._DEVICES_TYPE], _data._DEVICES_TYPE, None] = None,
-    platform: typing.Union[tuple[_data._PLATFORMS_TYPE], _data._PLATFORMS_TYPE, None] = None,
-    browser: typing.Union[tuple[_data._BROWSERS_TYPE], _data._BROWSERS_TYPE, None] = None,
-    options: typing.Union[_options.Options, None] = None
-):
+def generate(device: typing.Union[tuple, str, None] = None,
+             platform: typing.Union[tuple, str, None] = None,
+             browser: typing.Union[tuple, str, None] = None,
+             options: typing.Union[_options.Options, None] = None) -> user_agent.UserAgent:
     return user_agent.UserAgent(device, platform, browser, options)
