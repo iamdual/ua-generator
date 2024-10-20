2a5469337ae814d648cb54fcbb35a49c7f3d1abb
ae <85891169+novitae@users.noreply.github.com>
2024-10-20 13:29:09 +0200

    typing + repr

--- src/ua_generator/data/__init__.py
+++ src/ua_generator/data/__init__.py
@@ -3,12 +3,17 @@ Random User-Agent
 Copyright: 2022-2024 Ekin Karadeniz (github.com/iamdual)
 License: Apache License 2.0 
 """
+from typing import Literal
 
 DEVICES = ('desktop', 'mobile')
+_DEVICES_TYPE = Literal['desktop', 'mobile', None]
 
 PLATFORMS = ('windows', 'macos', 'ios', 'linux', 'android')
+_PLATFORMS_TYPE = Literal['windows', 'macos', 'ios', 'linux', 'android', None]
 PLATFORMS_DESKTOP = ('windows', 'macos', 'linux')  # Platforms on desktop devices
 PLATFORMS_MOBILE = ('ios', 'android')  # Platforms on mobile devices
 
 BROWSERS = ('chrome', 'edge', 'firefox', 'safari')
+_BROWSERS_TYPE = Literal['chrome', 'edge', 'firefox', 'safari', None]
 BROWSERS_SUPPORT_CH = ('chrome', 'edge')  # Browsers that support Client Hints
+_BROWSERS_SUPPORT_CH_TYPE = Literal['chrome', 'edge', None]
