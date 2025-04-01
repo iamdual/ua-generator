a4e303310b923a2a3e403106217f106b7c6420df
iamdual <imduual@gmail.com>
2025-04-01 15:40:06 +0300

    fix: type checking in version_range()

--- src/ua_generator/data/filterer.py
+++ src/ua_generator/data/filterer.py
@@ -7,8 +7,11 @@ class Filterer:
     def __init__(self, versions):
         self.versions = versions
 
-    def version_range(self, version_ranges: VersionRange):
-        filtered = version_ranges.filter(self.versions)
+    def version_range(self, v_range: VersionRange):
+        if not isinstance(v_range, VersionRange):
+            raise TypeError('Parameter v_range must be type of VersionRange')
+
+        filtered = v_range.filter(self.versions)
         if len(filtered) > 0:
             self.versions = filtered
 
@@ -17,4 +20,5 @@ class Filterer:
             weights = [1.0] * len(self.versions)
             for i in range(1, min(max_range, len(self.versions))):
                 weights[-i] = 10.0 - i
+
         self.versions = random.choices(self.versions, weights=weights, k=1)
