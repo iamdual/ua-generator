70591137cc14661d572392ea32b2a27df80a705b
iamdual <imduual@gmail.com>
2025-08-17 00:06:08 +0300

    implement merge_versions()

--- scripts/update_bot/updaters/__init__.py
+++ scripts/update_bot/updaters/__init__.py
@@ -9,6 +9,7 @@ class Updater(object):
     def __init__(self):
         self.versions = []
         self.current = []
+        self.merged = []
 
     def get_title(self):
         title = self.__class__.__name__.removesuffix("Updater")
@@ -21,20 +22,34 @@ class Updater(object):
     def fetch_versions(self):
         pass
 
+    def merge_versions(self):
+        merged = {}
+
+        for major, minor, build in self.current + self.versions:
+            key = (major, minor)
+            if key not in merged or build > merged[key]:
+                merged[key] = build
+
+        self.merged = [(major, minor, build) for (major, minor), build in sorted(merged.items())]
+
     def update(self):
         print("Updating {}".format(self.get_title()))
         self.fetch_current()
         self.fetch_versions()
+        self.merge_versions()
 
     def info(self):
         print("{} version info".format(self.get_title()))
         self.fetch_current()
         self.fetch_versions()
+        self.merge_versions()
 
         print("Current versions:")
         print(self.current)
         print("Recent versions:")
         print(self.versions)
+        print("Merged versions:")
+        print(self.merged)
         print("\n")
 
     def action(self, action):
