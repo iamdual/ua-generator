9cf142d600a21599fff46a420ab5009375d05d07
iamdual <imduual@gmail.com>
2025-08-18 15:11:16 +0300

    implement the version data update

--- cli/update_bot/updaters/macos.py
+++ cli/update_bot/updaters/macos.py
@@ -3,8 +3,8 @@ Random User-Agent
 Copyright: 2025 Ekin Karadeniz (github.com/iamdual)
 License: Apache License 2.0
 """
+import re
 import requests
-
 from . import Updater
 
 
@@ -28,3 +28,23 @@ class MacOSUpdater(Updater):
         from src.ua_generator.data.platforms.macos import VERSIONS
         for version in VERSIONS:
             self.current.append((version.major, version.minor, version.build))
+
+    def update(self):
+        super().update()
+        name = "platforms/macos.py"
+
+        src = self.get_data_content(name)
+        src_match = re.search(
+            r"(VERSIONS:\s+List\[Version\]\s*=\s*\[\n)(.*?)(\n\s*\])",
+            src,
+            flags=re.DOTALL,
+        )
+        assert src_match
+        prefix, _, suffix = src_match.groups()
+
+        body = ""
+        for major, minor, build in self.merged:
+            build_arg = f"(0, {build})" if build > 0 else "0"
+            body += f"    Version(major={major}, minor={minor}, build={build_arg}),\n"
+
+        self.set_data_content(name, src[:src_match.start()] + prefix + body.rstrip() + suffix + src[src_match.end():])
