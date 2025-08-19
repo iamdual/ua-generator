95645f01421d0b883dbc86ceaae47aa2bc475f09
iamdual <imduual@gmail.com>
2025-08-20 00:48:26 +0300

    use lists in examples

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -11,30 +11,34 @@ import src.ua_generator as ua_generator
 class TestBrowser(unittest.TestCase):
     def test_browser(self):
         for i in range(0, 100):
-            ua = ua_generator.generate(browser=('chrome',))
+            ua = ua_generator.generate(browser=['chrome'])
             self.assertTrue(ua.browser == 'chrome')
 
     def test_browser_2(self):
         for i in range(0, 100):
-            ua = ua_generator.generate(browser=('chrome', 'firefox'))
+            ua = ua_generator.generate(browser=['chrome', 'firefox'])
             self.assertTrue(ua.browser == 'chrome' or ua.browser == 'firefox')
 
     def test_browser_3(self):
         for i in range(0, 100):
-            ua = ua_generator.generate(platform=('macos', 'ios'), browser='safari')
+            ua = ua_generator.generate(platform=['macos', 'ios'], browser='safari')
             self.assertTrue(ua.browser == 'safari')
 
     def test_browser_4(self):
         for i in range(0, 100):
-            ua = ua_generator.generate(platform=('windows', 'linux'), browser='safari')
+            ua = ua_generator.generate(platform=['windows', 'linux'], browser='safari')
             self.assertTrue(ua.browser == 'chrome')
 
     def test_browser_5(self):
         for i in range(0, 100):
-            ua = ua_generator.generate(platform=('macos', 'linux'), browser='safari')
+            ua = ua_generator.generate(platform=['macos', 'linux'], browser='safari')
             self.assertTrue((ua.platform == 'macos' and ua.browser == 'safari') or
                             (ua.platform == 'linux' and ua.browser == 'chrome'))
 
+    def test_browser_tuples(self):
+        for i in range(0, 100):
+            ua = ua_generator.generate(browser=('chrome', 'firefox'))
+            self.assertTrue(ua.browser == 'chrome' or ua.browser == 'firefox')
 
 if __name__ == '__main__':
     unittest.main()
