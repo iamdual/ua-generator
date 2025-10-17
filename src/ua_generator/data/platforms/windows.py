d0c7de4842dc1bb73eaa3cbb6fbdafbf695de123
iamdual <imduual@gmail.com>
2025-10-17 10:08:15 +0300

    fix platform versions of Windows

--- src/ua_generator/data/platforms/windows.py
+++ src/ua_generator/data/platforms/windows.py
@@ -13,11 +13,11 @@ from ...options import Options
 # https://learn.microsoft.com/en-us/windows/win32/sysinfo/operating-system-version
 # https://learn.microsoft.com/en-us/microsoft-edge/web-platform/how-to-detect-win11
 VERSIONS: List[WindowsVersion] = [
-    WindowsVersion(Version(major=6, minor=1), ch_platform=Version(major=7)),
-    WindowsVersion(Version(major=6, minor=2), ch_platform=Version(major=8)),
-    WindowsVersion(Version(major=6, minor=3), ch_platform=Version(major=8, minor=1)),
-    WindowsVersion(Version(major=10, minor=0), ch_platform=Version(major=10)),
-    WindowsVersion(Version(major=10, minor=0), ch_platform=Version(major=(13, 15))),
+    WindowsVersion(Version(major=6, minor=1), ch_platform=Version(major=0)),  # Win7
+    WindowsVersion(Version(major=6, minor=2), ch_platform=Version(major=0)),  # Win8
+    WindowsVersion(Version(major=6, minor=3), ch_platform=Version(major=0)),  # Win8.1
+    WindowsVersion(Version(major=10, minor=0), ch_platform=Version(major=(1, 10))),  # Win10
+    WindowsVersion(Version(major=10, minor=0), ch_platform=Version(major=(13, 15))),  # Win11
 ]
 
 
